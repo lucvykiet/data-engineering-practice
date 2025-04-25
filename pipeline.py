bc143c847f6c021931b93b7e356991f531d9afb1
lucvykiet <lucvykiet@gmail.com>
2025-04-25 09:08:08 +0700

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -9,8 +9,8 @@ def run_step(name, path):
     print(f"✅ {name} hoàn thành")
 
 if __name__ == "__main__":
-    run_step("Bài tập 1", "Bài tập 1/main.py")
-    run_step("Bài tập 2", "Bài tập 2/main.py")
-    run_step("Bài tập 3", "Bài tập 3/main.py")
+    run_step("Bài tập 1", "Exercise-1/main.py")
+    run_step("Bài tập 2", "Exercise-2/main.py")
+    run_step("Bài tập 3", "Exercise-3/main.py")
     run_step("Bài tập 4", "Exercise-4/main.py")
-    run_step("Bài tập 5", "Bài tập 5/main.py")
+    run_step("Bài tập 5", "Exercise-5/main.py")
